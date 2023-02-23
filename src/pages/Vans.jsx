99c9b7d5ba3549842478195c77876d3793b17c76
diegoram23 <diego.pta.oc@gmail.com>
2023-02-23 13:49:59 -0800

    vans page css styling

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -8,25 +8,22 @@ export default function Vans() {
         console.log('effect ran')
         fetch("/api/vans")
             .then(res => res.json())
-            .then(data => {
-                console.log(data.vans)
-                setVans(data.vans)
-            })
+            .then(data => setVans(data.vans))
     }, [])
 
     const vanElements = vans.map(van => (
-        <div key={van.id} className="van-content">
+        <div key={van.id} className="van-tile">
             <img src={van.imageUrl} />
             <div className="van-info">
                 <h2>{van.name}</h2>
-                <h2>{van.price}<span>/day</span></h2>
+                <h2>${van.price}<span>/day</span></h2>
             </div>
-            <i className="van-type">{van.type}</i>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
         </div>
     ))
 
     return (
-        <div className="van-container">
+        <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list">
                 {vanElements}
