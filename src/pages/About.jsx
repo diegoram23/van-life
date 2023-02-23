76ae80a09a15e8b46e657e08cd99ceb08254b256
diegoram23 <diego.pta.oc@gmail.com>
2023-02-23 11:09:24 -0800

    about page layout fundamentals completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,22 @@ import { Link } from 'react-router-dom'
 
 export default function About() {
     return (
-      <h1>About Page</h1>
+        <main className="about-page-container">
+            <img src={vanImg} className='about-hero-image' />
+            <section className="about-page-content">
+                <h1>Don't squeeze in a sedan when you could relax in a van.</h1>
+                <p>Our mission is to enliven your road trip with the perfect
+                    travel van rental. Our vans are recertified before each trip to ensure
+                    your travel plans can go off without a hitch.
+                </p>
+                <p>Our team is full of vanlife enthusiasts who know firsthand
+                    the magic of touring the world on 4 wheels.
+                </p>
+            </section>
+            <section className="about-page-cta">
+                <h2>Your destination is waiting.<br />Your van is ready.</h2>
+                <Link to="vans" className="link-button">Explore our vans</Link>
+            </section>
+        </main>
     )
-  }
-  
+}
